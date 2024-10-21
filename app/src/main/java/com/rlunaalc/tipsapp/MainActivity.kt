074e2f920bc5274a5f72b7996f95e75d7d95afab
paupc <p.pulido.c@institutvidreres.cat>
2024-10-21 13:50:56 +0200

    Add binding on buildgrale, layout on xml, and do logic on mainActivity

--- app/src/main/java/com/rlunaalc/tipsapp/MainActivity.kt
+++ app/src/main/java/com/rlunaalc/tipsapp/MainActivity.kt
@@ -9,40 +9,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.rlunaalc.tipsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        var input: TextView = findViewById(R.id.editTextText);
-        var finalPrice: TextView = findViewById(R.id.textView3)
-        val button: Button = findViewById(R.id.button);
-        var switch: Switch = findViewById(R.id.switch1);
+        val butonCalcular = binding.buttonCalc
 
-        //radiobuttons
-
-        val radio20: RadioButton = findViewById(R.id.radioButton);
-        val radio18: RadioButton = findViewById(R.id.radioButton2);
-        val radio15: RadioButton = findViewById(R.id.radioButton3);
-
-        //recollir dades
-
-        //input.text devuelve un charSequence, por eso se pasa a string
-        var price = input.text.toString().toDouble()
-
-        button.setOnClickListener{
-            if (radio20.isActivated){
-                finalPrice.setText(price* 0.2)
+            butonCalcular.setOnClickListener{
+                val precioInicial: String = binding.editTextText.text.toString()
+                val coste = when {
+                    binding.radioAmazing20.isChecked -> precioInicial.toDouble()*0.2
+                    binding.radioButtonGood18.isChecked -> precioInicial.toDouble()*0.18
+                    binding.radioButtonOkey15.isChecked -> precioInicial.toDouble()*0.15
+                    else -> {
+                        precioInicial.toDouble()
+                    }
+                }
+            binding.textViewPriceFinal.text = coste.toString()
             }
-
         }
 
-    }
+
 }
